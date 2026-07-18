ef3ae05f24c8ca8ef857fd814d94b1db98a99358
elbetel24 <elbethelshiferaw@gmail.com>
2026-07-18 15:39:54 +0300

    Spelling issue

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app=express();
 
 const allowedOrigins = [
   "http://localhost:5173",
- " https://movie-watchlist-tmdb.vercel.app/",
+ "https://movie-watchlist-tmdb.vercel.app",
 ];
 
 app.use(
