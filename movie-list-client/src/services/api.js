e99448248344931bf8acb72942bd6b4c7ad9a58e
elbetel24 <elbethelshiferaw@gmail.com>
2026-07-18 14:39:01 +0300

    updated the connection to back

--- movie-list-client/src/services/api.js
+++ movie-list-client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://movie-watchlist-app-u2fw.onrender.com/",
   withCredentials: true, // important if you're using cookies later
 
 });
