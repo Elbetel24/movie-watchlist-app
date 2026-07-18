75850723c3a28b696b131db67ed9cac2a50617af
elbetel24 <elbethelshiferaw@gmail.com>
2026-07-18 14:49:50 +0300

    Changed environment variables

--- movie-list-client/src/services/api.js
+++ movie-list-client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://movie-watchlist-app-u2fw.onrender.com/",
+  baseURL: import.meta.env.VITE_API_URL
   withCredentials: true, // important if you're using cookies later
 
 });
