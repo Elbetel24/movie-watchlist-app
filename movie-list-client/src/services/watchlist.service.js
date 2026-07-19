94f8c38547fa6267234c440b7c146943edf7ccd2
elbetel24 <elbethelshiferaw@gmail.com>
2026-07-19 12:46:25 +0300

    Fixed the frontend problems

--- movie-list-client/src/services/watchlist.service.js
+++ movie-list-client/src/services/watchlist.service.js
@@ -8,22 +8,22 @@ const client = axios.create({
 });
 
 const getWatchlist = async () => {
-  const res = await client.get('/watchlist');
+  const res = await client.get('/v1/watchlist');
   return res.data;
 };
 
 const addToWatchlist = async (movie) => {
-  const res = await client.post('/watchlist', movie);
+  const res = await client.post('/v1/watchlist', movie);
   return res.data;
 };
 
 const removeFromWatchlist = async (id) => {
-  const res = await client.delete(`/watchlist/${id}`);
+  const res = await client.delete(`/v1/watchlist/${id}`);
   return res.data;
 };
 
 const updateWatchlist = async (id, patch) => {
-  const res = await client.put(`/watchlist/${id}`, patch);
+  const res = await client.put(`/v1/watchlist/${id}`, patch);
   return res.data;
 };
 
